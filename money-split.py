a734bbbb36845c1032e7f2522fe94630cffd151b
Blake <107324569+blakerossi@users.noreply.github.com>
2022-07-21 13:46:25 -0700

    Fixing structure, variable names, add docstrings
    Got some help from people on changes to make the code better, and improve readability.

--- money-split.py
+++ money-split.py
@@ -1,51 +1,49 @@
-numbers = input("What dollar amounts are you starting with? please enter each amount seperated by a space: ")
-ending_am = input("What is your final dollar amount? ")
-starting_amounts = numbers.split(" ") 
-number_of_splits = len(starting_amounts)
-ending_amount = int(ending_am)
-
-# converting all the strings in the starting_amounts array into integers
 def strings_to_floats(*starting_amounts):
-    int_starting_amounts = []
+    """converting all the strings in the starting_amounts list into floats"""
+    float_starting_amounts = []
     for i in starting_amounts:
         try:
-            int_starting_amounts.append(float(i))
+            float_starting_amounts.append(float(i))
         except ValueError:
             pass    
-    return int_starting_amounts
+    return float_starting_amounts
 
-int_starting_amounts = strings_to_floats(*starting_amounts)
 
-# calculating the starting_ammounts into a total
-def starting_amount_total(*int_starting_amounts):
-    total = 0
-    for i in int_starting_amounts:
-        total += i
-    return total
+def starting_amount_total(*float_starting_amounts):
+    return sum(float_starting_amounts)
 
-starting_total = starting_amount_total(*int_starting_amounts)
 
-#calculating the ratio of a given integer in the int_starting_ammounts array to the starting_total
 def ratio_to_total(int):
+    """calculating the ratio of a given integer in the int_starting_ammounts list to the starting_total"""
     ratio = int/starting_total
     return ratio
 
-#creating an array of ratios from each starting_ammount to the starting_total
-def ratios_of_starting_amounts(*int_starting_amounts):
-    ratios_arr = []
-    for i in int_starting_amounts:
+
+def ratios_of_starting_amounts(*float_starting_amounts):
+    """creating a list of ratios from each starting_ammount to the starting_total"""
+    ratios_list = []
+    for i in float_starting_amounts:
         ratio = ratio_to_total(i)
-        ratios_arr.append(ratio)
-    return ratios_arr
+        ratios_list.append(ratio)
+    return ratios_list
 
-ratios_array = ratios_of_starting_amounts(*int_starting_amounts)
-
-#this function creates an array of the ratios applied to the ending total
-def money_split(ending_amount, *ratios_array):
+def money_split(ending_amount, *ratios_list):
+    """this function creates a list of the ratios applied to the ending total"""
     ending_tot = []
-    for i in ratios_array:
+    for i in ratios_list:
         ending_tot.append(ending_amount * i)
     return ending_tot
 
-ending_totals = money_split(ending_amount, *ratios_array)
+
+if __name__ == "__main__":
+
+    numbers = input("What dollar amounts are you starting with? please enter each amount seperated by a space: ")
+    ending_am = input("What is your final dollar amount? ")
+    starting_amounts = numbers.split(" ") 
+    ending_amount = int(ending_am)
+    float_starting_amounts = strings_to_floats(*starting_amounts)
+    starting_total = starting_amount_total(*float_starting_amounts)
+    ratios_list = ratios_of_starting_amounts(*float_starting_amounts)
+    ending_totals = money_split(ending_amount, *ratios_list)
+
 print(ending_totals)
